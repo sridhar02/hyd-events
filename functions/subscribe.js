bc0f05ded13e076bd774e3152be642886354132f
sridhar02 <kattasridhar02@gmail.com>
2023-09-04 22:02:38 +0530

    add post CF function

---
+++ functions/subscribe.js
@@ -0,0 +1,31 @@
+export async function onRequest({ request, env }) {
+  return await submitHandler({ request, env });
+}
+
+async function submitHandler({ request, env }) {
+  const body = await request.formData();
+
+  console.log(body);
+
+  const { email } = Object.fromEntries(body);
+
+  const reqBody = {
+    email: email,
+    status: "enabled",
+    name: "The Subscriber",
+    lists: [2],
+  };
+
+  return handleSubscriber({ body: reqBody });
+}
+
+async function handleSubscriber(body) {
+  return fetch("https://api.ksridhar.dev/api/subscribers", {
+    method: "POST",
+    body: JSON.stringify(body),
+    headers: {
+      Authorization: `Basic bGlzdG1vbms6bGlzdG1vbms=`,
+      "Content-type": `application/json`,
+    },
+  });
+}
